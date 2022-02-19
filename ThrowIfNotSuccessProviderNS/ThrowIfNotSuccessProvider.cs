a85534fa9a81d4ba68691cb49b62c3f6de7ad022
ivanivanyuk1993 <ivanivanyuk1993@gmail.com>
2022-02-19 12:36:56 +0300

    Added `ThrowIfNotSuccessProvider` to ease logging of cli errors
    Added `RenameRepositoryListInGithubProvider` to ease renaming multiple github projects

---
+++ ThrowIfNotSuccessProviderNS/ThrowIfNotSuccessProvider.cs
@@ -0,0 +1,15 @@
+﻿using CliExitCodeProviderNS;
+using ExecuteCliCommandResultNS;
+
+namespace ThrowIfNotSuccessProviderNS;
+
+public static class ThrowIfNotSuccessProvider
+{
+    public static void ThrowIfNotSuccess(this ExecuteCliCommandResult executeCliCommandResult)
+    {
+        if (!executeCliCommandResult.ExitCode.IsSuccessfulCliExitCode())
+        {
+            throw new Exception(message: executeCliCommandResult.StandardErrorOutputText);
+        }
+    }
+}
